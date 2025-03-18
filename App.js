dd416cc879c7120de1f474cfb80fab1e2f9eaeb0
Paras <paraschaudhary176@gmail.com>
2025-03-18 21:23:11 +0530

    core way of writing react code

--- App.js
+++ App.js
@@ -1,5 +1,29 @@
-const heading = React.createElement("h1", {}, "Hello World From React");
+/*
+<div id="Parent">
+<div id="Child">
+<h1></h1>
+</div>
+</div>
+*/
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const _heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World From react"
+// );
+// const _root = ReactDOM.createRoot(document.getElementById("root"));
+// _root.render(_heading);
 
-root.render(heading);
+const parent = React.createElement(
+  "div",
+  { id: "Parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
+);
+
+const _root = ReactDOM.createRoot(document.getElementById("root"));
+
+_root.render(parent);
